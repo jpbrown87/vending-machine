20e176d241a29f537b3163214f85a65055e51764
jpbrown87 <jpbrown87@gmail.com>
2020-11-23 17:00:48 -0600

    Stopped at making change

--- src/Machine.js
+++ src/Machine.js
@@ -1,6 +1,7 @@
 class Machine {
   constructor() {
     this.snacks = []
+    this.balance = 0;
   }
 
   seeSelections() {
@@ -12,6 +13,57 @@ class Machine {
       throw Error("please do not troll. you cannot stock nothing.")
     }
     this.snacks = inventory
+  }
+  
+  deposit(number){
+    if(number !== 10 && number !== 20 && number !==50 && number !== 100 && number !== 500){
+      throw Error("Invalid bill inserted!")
+    } 
+    this.balance += number;
+
+    return `You have deposited Rs ${this.balance}`
+  } 
+  selectItem(code){
+    const snack = this.snacks[code];
+    if(!snack){
+      return "The item you selected is unavailable";
+    }else if(this.balance < snack?.price){
+      return `Your deposit is insufficient.  Please add Rs ${snack.price - this.balance} for this item`;
+    }
+
+    let totalChange = this.balance - snack.price;
+    let change = [];
+
+    while(totalChange > 0){
+      console.log(totalChange >= 100)
+      switch(totalChange){
+        case totalChange >= 500:
+          change.push(500);
+          totalChange -= 500;
+          break;
+        case totalChange >= 100:
+          console.log("case 100 Ran")
+          change.push(100);
+          totalChange -= 100;
+          break;
+        case totalChange >= 50:
+          change.push(50);
+          totalChange -= 50;
+          break;
+        case totalChange >= 20:
+          change.push(20);
+          totalChange -= 20;
+          break;
+        case totalChange >= 10:
+          change.push(10);
+          totalChange -= 10;
+          break;
+        default:
+          throw Error("Leftover change!")
+      }
+    }
+
+    return { item: snack.name, change}
   }
 }
 
--- test/Machine.test.js
+++ test/Machine.test.js
@@ -1,10 +1,15 @@
 const Machine = require('../src/Machine')
 
 describe('The vending machine', () => {
+  let vendingMachine;
+  beforeEach(()=>{
+    vendingMachine = new Machine();
+  })
+
   it('is initialized with no items', () => {
     // SEAT
     // setup
-    const vendingMachine = new Machine();
+    
 
     // exercise & assert
     expect(vendingMachine.seeSelections()).toEqual([])
@@ -14,7 +19,7 @@ describe('The vending machine', () => {
 
   it('can stock one snack', () => {
     // setup
-    const vendingMachine = new Machine();
+    
     const snack = {
       name: 'macadamia nuts',
       price: 250
@@ -41,10 +46,86 @@ describe('The vending machine', () => {
 
   it('displays an error if no inventory comes with stocking', () => {
     // setup
-    const vendingMachine = new Machine()
+    
     const displayMessage = "please do not troll. you cannot stock nothing."
 
     // exercise & assert
     expect(() => vendingMachine.stock()).toThrow(displayMessage)
+  })
+  it('shows total deposited', () => {
+    
+    const expected = 'You have deposited Rs 100'
+
+    expect(vendingMachine.deposit(100)).toEqual(expected)
+  })
+
+  describe('accepts only 10, 20 50, 100, 500 bills',()=>{
+    it('rejects invalid bills',()=>{
+      const expectedError = "Invalid bill inserted!";
+      expect(()=>vendingMachine.deposit(30)).toThrow(expectedError)
+      expect(()=>vendingMachine.deposit(150)).toThrow(expectedError)
+      expect(()=>vendingMachine.deposit(1000)).toThrow(expectedError)
+      expect(()=>vendingMachine.deposit(300)).toThrow(expectedError)
+      expect(()=>vendingMachine.deposit(25)).toThrow(expectedError)
+    })
+
+    it('accepts all valid bills',()=>{
+      const expectedError = "Invalid bill inserted!";
+
+      expect(()=>vendingMachine.deposit(10)).not.toThrow(expectedError)
+      expect(()=>vendingMachine.deposit(20)).not.toThrow(expectedError)
+      expect(()=>vendingMachine.deposit(50)).not.toThrow(expectedError)
+      expect(()=>vendingMachine.deposit(100)).not.toThrow(expectedError)
+      expect(()=>vendingMachine.deposit(500)).not.toThrow(expectedError)
+    })
+  })
+
+  describe('vending machine should keep total of deposited money',() => {
+    it('adds money that is deposited',() => { 
+
+      expect(vendingMachine.deposit(10)).toEqual("You have deposited Rs 10")
+      expect(vendingMachine.deposit(20)).toEqual("You have deposited Rs 30")
+      expect(vendingMachine.deposit(50)).toEqual("You have deposited Rs 80")
+    })
+  })
+
+  describe('tells customer when item is unavailable', ()=>{
+    it('displays "The item you selected is unavailable"',()=>{
+      const expected = 'The item you selected is unavailable';
+      const code = 0;
+      expect(vendingMachine.selectItem(code)).toEqual(expected)
+    })
+  }) 
+  
+  describe('Customer receives notification if balance is insufficient', ()=>{
+    it('returns notifcation that balance is insufficient',()=>{
+      const code = 0;
+      const snack = {
+        name: 'macadamia nuts',
+        price: 250
+      }
+      vendingMachine.stock([snack])
+      
+      expect(vendingMachine.selectItem(code)).toEqual("Your deposit is insufficient.  Please add Rs 250 for this item")
+      vendingMachine.deposit(100)
+      expect(vendingMachine.selectItem(code)).toEqual("Your deposit is insufficient.  Please add Rs 150 for this item")
+    })
+  }) 
+
+  describe('Receive change when customer has overpaid', ()=>{
+    it('dispenses change when customer has overpaid',()=>{
+      const code = 0;
+      const expected = {
+        item: 'macadamia nuts', 
+        change: [100, 100, 50]
+      };
+      const snack = {
+        name: 'macadamia nuts',
+        price: 250
+      }
+      vendingMachine.stock([snack])
+      vendingMachine.deposit(500)
+      expect(vendingMachine.selectItem(code)).toEqual(expected)
+    })
   })
 })
